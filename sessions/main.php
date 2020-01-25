07fa6fb58a2080c6d3ec92b251758632217624c1
Ng'ang'a Victor <ngangavictor10@gmail.com>
2020-01-25 19:47:21 +0300

    main

--- sessions/main.php
+++ sessions/main.php
@@ -1,6 +1,57 @@
 <?php
 session_start();
 
-echo 'Session ID is: '.$_SESSION['id'];
-echo '</br>';
-echo 'The phone number is: '.$_SESSION['phone'];
+require "mombasa.php";
+require "kisumu.php";
+
+
+if(isset($_GET['text'])&&!empty($_GET['text'])){
+$code=$_GET['text'];
+
+// if(checkText($code)){
+switch($code){
+    case "1":
+    //msa
+    // header("location: boarding.php?code=1");
+    require "boarding.php";
+     boarding("1");
+
+    break;
+    case "2":
+    //kisumu
+    break;
+    case "3":
+    //nairobi
+    break;
+    case "1*1":
+    //dest Nairobi
+    require "nairobi.php";
+    // toNairobi();
+    break;
+    default:
+    $output="Choose boarding point.</br>";
+    $output.="1. Mombasa</br>";
+    $output.="2. Nairobi</br>";
+    $output.="3. Kisumu";
+    echo $output;
+}
+
+// }else{
+//     $output="Choose boarding point.</br>";
+//     $output.="1. Mombasa</br>";
+//     $output.="2. Nairobi</br>";
+//     $output.="3. Kisumu";
+//     echo $output;
+// }
+
+}else{
+    $output="Choose boarding point.</br>";
+    $output.="1. Mombasa</br>";
+    $output.="2. Nairobi</br>";
+    $output.="3. Kisumu";
+    echo $output;
+}
+
+// function checkText($code){
+//     return is_numeric($code);
+// }
