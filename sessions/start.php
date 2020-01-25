af52865779025e968869500e39db5f7d89f89ae9
Ng'ang'a Victor <ngangavictor10@gmail.com>
2020-01-25 14:02:05 +0300

    verify phone

--- sessions/start.php
+++ sessions/start.php
@@ -2,21 +2,31 @@
 /****
  * application starts here
  * check if phone number is given
- * import the database connection
+ * verify phone number
  * set sessions i.e. id & phone
  * ****/
 session_start();
-require "../connection.php";
 
 if(isset($_GET['phonenumber'])&&!empty($_GET['phonenumber'])){
 $phone=$_GET['phonenumber'];
 
-$_SESSION['id']=md5($phone);
-$_SESSION['phone']=$phone;
-
-header("location: main.php");
+if(verifyPhone($phone)){
+    $_SESSION['id']=md5($phone);
+    $_SESSION['phone']=$phone;
+    
+    header("location: main.php");
+}else{
+    echo 'Invalid phone number';
+}
 
 }else{
     echo 'Application error';
+}
+
+function verifyPhone($phone){
+if(strlen($phone)>=10)
+  return true;
+else
+  return false;
 }
 
